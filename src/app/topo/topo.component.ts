ad327ef2380ebeefa18bf1d0a675e16511d40b21
Neto Castro <netocastrotec@gmail.com>
2019-11-12 09:49:51 -0200

    primeiro app curso de  angular

---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-topo',
+    styleUrls: ['./topo.component.css'],
+    templateUrl: './topo.component.html'
+})
+export class TopoComponent{ }
